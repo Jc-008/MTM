27018083488cfe83e538dee4a22c886321880f22
jonathan <jonathanchan.808@gmail.com>
2021-06-03 13:52:27 -0400

    Fixed seed data issue on user

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -2,10 +2,12 @@ from werkzeug.security import generate_password_hash
 from app.models import db, User
 
 # Adds a demo user, you can add other users here if you want
+
+
 def seed_users():
 
-    demo = User(username='Demo', email='demo@aa.io',
-                password='password')
+    demo = User(username='Demo', email='demo@aa.io', zipcode='10001',
+                password='password', is_owner=False)
 
     db.session.add(demo)
 
@@ -15,6 +17,8 @@ def seed_users():
 # SQLAlchemy doesn't have a built in function to do this
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
+
+
 def undo_users():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.commit()
