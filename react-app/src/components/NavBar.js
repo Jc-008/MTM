f439090764597e017ef3d66ac1d879dafed90d33
jonathan <jonathanchan.808@gmail.com>
2021-06-03 19:17:35 -0400

    working on styling searchbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import SearchBarForm from '../components/SearchbarForm'
 import {
   Wrap,
   WrapItem,
@@ -23,27 +24,24 @@ import {
 
 const NavBar = () => {
   return (
-    <>
-      <Grid templateColumns="repeat(11, 1fr)" gap={.7} bg="#FFFFF">
-        <Flex >
-          <Link href='/' w="100%" h="55px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >MTM</Link>
-        </Flex>
-        <Flex w="100%" h="55px" />
-        {/* <SearchBar/> */}
-        <Flex w="100%" h="55px" />
-        <Flex w="100%" h="55px" />
-        <Flex w="100%" h="55px" />
-        <Flex w="100%" h="55px" />
-        <Flex w="100%" h="55px" />
-        <Flex w="100%" h="55px" />
-        <Link href='' w="100%" h="55px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >How it works</Link>
-        <Link href='' w="100%" h="55px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Locations</Link>
-        {/* <Button href='' w="100%" h="25px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Try MTM</Button> */}
-        <Flex alignItems='center' justifyContent='center'>
-          <Button href='' w='100px' bg='#0055FF' color='white' _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Try MTM</Button>
-        </Flex>
-      </Grid >
-    </>
+    <Flex>
+      <Flex w="100%" h="55px">
+        <Link href='/' w="100%" h="55px" textAlign="center" paddingTop={5} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >MTM</Link>
+      </Flex>
+      <Flex w="100%" h="55px">
+        <SearchBarForm />
+      </Flex>
+      <Flex w="100%" h="55px" />
+      <Flex w="100%" h="55px" />
+      <Flex w="100%" h="55px" />
+      <Flex w="100%" h="55px" />
+      <Link href='' w="100%" h="55px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >How it works</Link>
+      <Link href='' w="100%" h="55px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Locations</Link>
+      {/* <Button href='' w="100%" h="25px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Try MTM</Button> */}
+      <Flex w="100%" h="55px" alignItems='center' justifyContent='center'>
+        <Button href='' w='100px' bg='#0055FF' color='white' _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Try MTM</Button>
+      </Flex>
+    </Flex>
   );
 }
 
@@ -51,30 +49,22 @@ export default NavBar;
 
 
 
-{/* <nav>
-  <ul>
-    <li>
-      <NavLink to="/" exact={true} activeClassName="active">
-        Home
-          </NavLink>
-    </li>
-    <li>
-      <NavLink to="/login" exact={true} activeClassName="active">
-        Login
-          </NavLink>
-    </li>
-    <li>
-      <NavLink to="/sign-up" exact={true} activeClassName="active">
-        Sign Up
-          </NavLink>
-    </li>
-    <li>
-      <NavLink to="/users" exact={true} activeClassName="active">
-        Users
-          </NavLink>
-    </li>
-    <li>
-      <LogoutButton />
-    </li>
-  </ul>
-</nav> */}
+/* <>
+  <Grid templateColumns="repeat(11, 1fr)" gap={.7} bg="#FFFFF" minHeight={'64px'} >
+    <Flex >
+      <Link href='/' w="100%" h="55px" textAlign="center" paddingTop={5} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >MTM</Link>
+    </Flex>
+    <Flex w="100%" h="55px" />
+    <SearchBarForm />
+    <Flex w="100%" h="55px" />
+    <Flex w="100%" h="55px" />
+    <Flex w="100%" h="55px" />
+    <Flex w="100%" h="55px" />
+    <Link href='' w="100%" h="55px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >How it works</Link>
+    <Link href='' w="100%" h="55px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Locations</Link> */
+// {/* <Button href='' w="100%" h="25px" textAlign="center" paddingTop={4} fontWeight="bold" _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Try MTM</Button> */ }
+//     <Flex alignItems='center' justifyContent='center'>
+//       <Button href='' w='100px' bg='#0055FF' color='white' _hover={{ color: "white", fontWeight: "bold", bg: "gray.100" }} >Try MTM</Button>
+//     </Flex>
+//   </Grid >
+// </>
---
+++ react-app/src/components/SearchbarForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+// import * as searchActions from '../../store/search'
+
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  InputGroup,
+  isRequired,
+  Stack,
+  Flex,
+} from "@chakra-ui/react";
+
+
+const SearchBarForm = () => {
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const [searchTerm, setSearchTerm] = useState('')
+
+  // const handleSearch = async (e) => {
+  //   e.preventDefault();
+  //   setSearchTerm('')
+
+  //   await dispatch(searchActions.searchFunc(searchTerm))
+  //   history.push(`/search/${searchTerm}`)
+  // }
+
+
+  return (
+
+    <form onSubmit>
+      {/* <form onSubmit={handleSearch}> */}
+      <FormControl
+        w="300px" h="55" bg="#FFFFFF" color='#000000' textAlign="center" paddingTop={5}
+      >
+        <InputGroup>
+          <Input
+            placeholder="Find a gym or class"
+            bg="white"
+            type='text'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <Button type='Submit' colorScheme="white" variant="ghost" _hover={{ color: "white", fontWeight: "bold", bg: "gray.400" }}><i className="fas fa-search"></i></Button>
+          {/* <Button onClick={handleSearch} type='Submit' colorScheme="white" variant="ghost" _hover={{ color: "white", fontWeight: "bold", bg: "gray.400" }}><i className="fas fa-search"></i></Button> */}
+        </InputGroup>
+      </FormControl>
+    </form>
+  )
+}
+
+export default SearchBarForm;
