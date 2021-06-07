00ba1657d0d6e98a3bf74997f56f8e94d8a6f432
jonathan <jonathanchan.808@gmail.com>
2021-06-07 17:45:48 -0400

    Working on design and placement of components on this page

--- react-app/src/components/BusinessOwnerLogin.js
+++ react-app/src/components/BusinessOwnerLogin.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 // import { Redirect } from "react-router-dom";
 // import { login } from "../../store/session";
-import * as sessionActions from "../../store/session"
+import * as sessionActions from "../store/session"
 import {
   Box,
   Flex,
@@ -17,14 +17,14 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 
-export function BusinessOwnerLogin() {
+export default function BusinessOwnerLogin() {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
 
-  const handleSubmit = async (e) => {
+  const handle_Business_Submit = async (e) => {           // Only changed names for this
     e.preventDefault();
     setErrors([]);
     const dispatched = await dispatch(sessionActions.login(email, password))
@@ -32,73 +32,91 @@ export function BusinessOwnerLogin() {
     if (dispatched.errors) setErrors(dispatch.errors)
   };
 
+  const handle_Business_Demo = async (e) => {           // Only changed names for this
+    e.preventDefault();
+    const email = 'demo@aa.io';
+    const password = 'password'
+    const dispatched = await dispatch(sessionActions.login(email, password))
 
+    if (dispatched.errors) setErrors(dispatch.errors)
+  }
 
   return (
     <>
-      <Box
-        w={'450px'}
-        rounded={'lg'}
-        bg={useColorModeValue('white', 'gray.700')}
-        boxShadow={'0 4px 12px rgb(0 0 0 / 24%)'}
-        p={8}
-
+      <Flex
+        align={"center"}
+        justify={"center"}
+        h={'82vh'}
+        // mb={'100px'}
+        bg={'#f7f7f7'}
       >
-        <form onSubmit={handleSubmit}>
-          <div>
-            {errors.map((error, idx) => <span key={idx}>{error}</span>)}
-          </div>
-          <Stack spacing={3}>
-            <FormControl isRequired>
-              <FormLabel>Email</FormLabel>
-              <InputGroup>
-                <Input
-                  placeholder="Email"
-                  type='email'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </InputGroup>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input
-                  placeholder="Password"
-                  type='password'
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </InputGroup>
-              <br />
-              <Flex
-                align={"center"}
-                justify={"center"}
-              >
-                <Button type='Submit' bg='#0055FF' color='white' _hover={{ bg: '#004de6' }}>Log in</Button>
-              </Flex>
-            </FormControl>
-          </Stack>
-        </form>
-
-        <form onSubmit={handleDemo}>
-          <br />
-          <Flex
+        <Box
+          w={'450px'}
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'0 4px 12px rgb(0 0 0 / 24%)'}
+          p={8}
+          mt={'100px'}
+          h={'450px'}
+        >
+          <Text
             align={"center"}
             justify={"center"}
+            fontSize={'25px'}
+            fontWeight={'600'}
+            mb={'25px'}
           >
-            <Button type="Submit" bg='#0055FF' color='white' _hover={{ bg: '#004de6' }}>Demo Biz Owner</Button>
-          </Flex>
-        </form>
-        {/* <br />
-        <Flex
-          align={"center"}
-          justify={"flex-end"}
-        >
-          <Text fontSize={'15px'} color={'gray.600'}>
-            Business Owners, log in <Link href="/business-owner-login" color={'#0055FF'}>here</Link>
+            Welcome Back
           </Text>
-        </Flex> */}
+          <form onSubmit={handle_Business_Submit}>
+            <div>
+              {errors.map((error, idx) => <span key={idx}>{error}</span>)}
+            </div>
+            <Stack spacing={3}>
+              <FormControl isRequired>
+                <FormLabel>Email</FormLabel>
+                <InputGroup>
+                  <Input
+                    placeholder="Email"
+                    type='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </InputGroup>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    placeholder="Password"
+                    type='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </InputGroup>
+                <br />
+                <br />
+                <Flex
+                  align={"center"}
+                  justify={"center"}
+                  mb={'15px'}
+                >
+                  <Button type='Submit' bg='#0055FF' color='white' _hover={{ bg: '#004de6' }}>Log in</Button>
+                </Flex>
+              </FormControl>
+            </Stack>
+          </form>
 
-      </Box>
+          <form onSubmit={handle_Business_Demo}>
+            {/* <br /> */}
+            <Flex
+              align={"center"}
+              justify={"center"}
+            >
+              <Button type="Submit" bg='#0055FF' color='white' _hover={{ bg: '#004de6' }}>Demo Biz Owner</Button>
+            </Flex>
+          </form>
+        </Box>
+      </Flex>
+
 
     </>
   )
