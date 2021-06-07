563253e5e33a96b1736038e683cf4308211185ad
jonathan <jonathanchan.808@gmail.com>
2021-06-07 12:31:27 -0400

    Working on footer idea and design

---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
+import { ReactNode } from 'react';
+
+
+
+export default function FooterDetails() {
+
+
+  return (
+    <Box
+      bg={useColorModeValue('#333333')}
+      color={useColorModeValue('#ffffff')}
+    >
+      Hello!!!!!
+    </Box>
+
+  )
+}
