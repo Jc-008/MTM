c32052aa8a9c982212319ec34152bedccf4d9595
jonathan <jonathanchan.808@gmail.com>
2021-06-07 21:01:26 -0400

    Starting on signup page and form

---
+++ react-app/src/components/SignupPage.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+// import { Redirect } from "react-router-dom";
+// import { login } from "../../store/session";
+import * as sessionActions from "../store/session"
+import SignUpForm from "./auth/SignUpForm";
+import {
+  Box,
+  Flex,
+  Link,
+  Text,
+  Image,
+  Input,
+  Stack,
+  Button,
+  FormLabel,
+  InputGroup,
+  FormControl,
+  useColorModeValue,
+  Container,
+} from "@chakra-ui/react";
+
+
+export default function SignUpPage() {
+
+
+  return (
+    <>
+      <Flex
+        align={"center"}
+        justify={"center"}
+        mb={'50px'}
+      >
+        <Box>
+          {/* <Text>This is where the login form will be</Text> */}
+          <SignUpForm />
+        </Box>
+      </Flex>
+
+    </>
+  )
+}
