acafff4f213396c33d255ea9a31520b2346bb2a5
jonathan <jonathanchan.808@gmail.com>
2021-06-08 12:33:26 -0400

    Updated users models and seeds to update based on signup requirements

--- app/models/user.py
+++ app/models/user.py
@@ -8,8 +8,10 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    # username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
     zipcode = db.Column(db.String(5), nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
     is_owner = db.Column(db.Boolean(), nullable=False)
@@ -31,7 +33,9 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             "id": self.id,
-            "username": self.username,
-            # "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "zipcode": self.zipcode,
+            # "username": self.username,
+            # "email": self.email,
         }
