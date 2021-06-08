b4990d445963f4c38a47b173600f805fd562b0d9
jonathan <jonathanchan.808@gmail.com>
2021-06-08 17:49:21 -0400

    Added userHomePage component to view if i am logged in or not

---
+++ react-app/src/components/UserHomePage.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import LoginForm from './auth/LoginForm'
+import {
+  name,
+  Wrap,
+  WrapItem,
+  Divider,
+  Box,
+  Image,
+  Container,
+  Center,
+  LinkBox,
+  LinkOverlay,
+  Heading,
+  Flex,
+  Text,
+} from "@chakra-ui/react"
+
+
+export default function UserHomePage() {
+
+  return (
+    <h1> This should be the search page</h1>
+  )
+
+}
