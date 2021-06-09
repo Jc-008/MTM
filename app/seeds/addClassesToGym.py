7ca07943808286ec9256e8a3036b24c1dacbf5db
jonathan <jonathanchan.808@gmail.com>
2021-06-09 13:47:36 -0400

    Seeding classes to gyms

--- app/seeds/addClassesToGym.py
+++ app/seeds/addClassesToGym.py
@@ -18,7 +18,93 @@ def seed_class_to_gym():
         classSession.id: classSession for classSession in classSession_list
     }
 
+    gym_dict[1].gymClasses.append(classSession_dict[7])
+    gym_dict[1].gymClasses.append(classSession_dict[3])
     gym_dict[1].gymClasses.append(classSession_dict[2])
-    # gym 1 gets class #2 added to it
+    # gym 1 gets class #7, 3, 2 added to it
+
+    gym_dict[2].gymClasses.append(classSession_dict[7])
+    gym_dict[2].gymClasses.append(classSession_dict[1])
+    gym_dict[2].gymClasses.append(classSession_dict[2])
+    gym_dict[2].gymClasses.append(classSession_dict[6])
+    gym_dict[2].gymClasses.append(classSession_dict[5])
+
+    gym_dict[3].gymClasses.append(classSession_dict[3])
+    gym_dict[3].gymClasses.append(classSession_dict[1])
+
+    gym_dict[4].gymClasses.append(classSession_dict[7])
+    gym_dict[4].gymClasses.append(classSession_dict[3])
+    gym_dict[4].gymClasses.append(classSession_dict[4])
+    gym_dict[4].gymClasses.append(classSession_dict[2])
+    gym_dict[4].gymClasses.append(classSession_dict[5])
+
+    gym_dict[5].gymClasses.append(classSession_dict[])
+    gym_dict[5].gymClasses.append(classSession_dict[])
+    gym_dict[5].gymClasses.append(classSession_dict[])
+
+    gym_dict[6].gymClasses.append(classSession_dict[])
+    gym_dict[6].gymClasses.append(classSession_dict[])
+    gym_dict[6].gymClasses.append(classSession_dict[])
+
+    gym_dict[7].gymClasses.append(classSession_dict[])
+    gym_dict[7].gymClasses.append(classSession_dict[])
+    gym_dict[7].gymClasses.append(classSession_dict[])
+    gym_dict[7].gymClasses.append(classSession_dict[])
+
+    gym_dict[8].gymClasses.append(classSession_dict[])
+    gym_dict[8].gymClasses.append(classSession_dict[])
+    gym_dict[8].gymClasses.append(classSession_dict[])
+    gym_dict[8].gymClasses.append(classSession_dict[])
+    gym_dict[8].gymClasses.append(classSession_dict[])
+
+    gym_dict[9].gymClasses.append(classSession_dict[])
+    gym_dict[9].gymClasses.append(classSession_dict[])
+    gym_dict[9].gymClasses.append(classSession_dict[])
+    gym_dict[9].gymClasses.append(classSession_dict[])
+
+    gym_dict[10].gymClasses.append(classSession_dict[])
+    gym_dict[10].gymClasses.append(classSession_dict[])
+    gym_dict[10].gymClasses.append(classSession_dict[])
+    gym_dict[10].gymClasses.append(classSession_dict[])
+
+    gym_dict[11].gymClasses.append(classSession_dict[])
+    gym_dict[11].gymClasses.append(classSession_dict[])
+    gym_dict[11].gymClasses.append(classSession_dict[])
+
+    gym_dict[12].gymClasses.append(classSession_dict[])
+    gym_dict[12].gymClasses.append(classSession_dict[])
+    gym_dict[12].gymClasses.append(classSession_dict[])
+
+    gym_dict[13].gymClasses.append(classSession_dict[])
+    gym_dict[13].gymClasses.append(classSession_dict[])
+    gym_dict[13].gymClasses.append(classSession_dict[])
+
+    gym_dict[14].gymClasses.append(classSession_dict[])
+    gym_dict[14].gymClasses.append(classSession_dict[])
+    gym_dict[14].gymClasses.append(classSession_dict[])
+    gym_dict[14].gymClasses.append(classSession_dict[])
+    gym_dict[14].gymClasses.append(classSession_dict[])
+
+    gym_dict[15].gymClasses.append(classSession_dict[])
+    gym_dict[15].gymClasses.append(classSession_dict[])
+    gym_dict[15].gymClasses.append(classSession_dict[])
+
+    gym_dict[16].gymClasses.append(classSession_dict[])
+    gym_dict[16].gymClasses.append(classSession_dict[])
+    gym_dict[16].gymClasses.append(classSession_dict[])
+
+    gym_dict[17].gymClasses.append(classSession_dict[])
+    gym_dict[17].gymClasses.append(classSession_dict[])
+    gym_dict[17].gymClasses.append(classSession_dict[])
+    gym_dict[17].gymClasses.append(classSession_dict[])
+
+    gym_dict[18].gymClasses.append(classSession_dict[])
+    gym_dict[18].gymClasses.append(classSession_dict[])
+    gym_dict[18].gymClasses.append(classSession_dict[])
+    gym_dict[18].gymClasses.append(classSession_dict[])
+
+    gym_dict[19].gymClasses.append(classSession_dict[])
+    gym_dict[19].gymClasses.append(classSession_dict[])
+    gym_dict[19].gymClasses.append(classSession_dict[])
 
     db.session.commit()
