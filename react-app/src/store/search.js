04372812d95f418f4d9a64dc2d2ad0d6cd99c33d
jonathan <jonathanchan.808@gmail.com>
2021-06-09 16:17:26 -0400

    Working on search while working on user homepage

---
+++ react-app/src/store/search.js
@@ -0,0 +1,39 @@
+const LOADRESULT = 'search/LOADRESULT'
+
+const loadResult = products => ({
+  type: LOADRESULT,
+  payload: products
+});
+
+
+
+
+
+export const searchFunc = (query) => async dispatch => {      // param is the searchTerm that a person searches for
+
+  const response = await fetch(`/api/search/${query}`);
+  // /api/search/shirt
+  if (response.ok) {
+    const searchResults = await response.json();    // searchResult = jsonifed response from the fetch
+    dispatch(loadResult(searchResults))
+    return searchResults;
+  }
+}
+
+
+//-------------------REDUCER--------------------------//
+const searchReducer = (state = {}, action) => {
+  let newState;
+
+  switch (action.type) {
+    case LOADRESULT:
+      newState = Object.assign({}, state)
+      newState.searchResults = action.payload
+      return newState
+
+    default:
+      return { ...state };
+  }
+}
+
+export default searchReducer;
