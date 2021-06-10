952ff8b4ac5ccf13d08f19ddb49c272bfa3fc62e
jonathan <jonathanchan.808@gmail.com>
2021-06-10 16:04:31 -0400

    Working on frontend rendering portion of it

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
+from app.forms import UpdateForm
 from app.models import User
 
 user_routes = Blueprint('users', __name__)
@@ -11,9 +12,37 @@ user_routes = Blueprint('users', __name__)
 #     users = User.query.all()
 #     return {"users": [user.to_dict() for user in users]}
 
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field} : {error}")
+    return errorMessages
+
 
 @user_routes.route('/<int:id>/')
 @login_required
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+
+@user_routes.route('/', method=['PUT'])
+# @login_required
+def update_user_details():
+    form = UpdateForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        user = User.query.get(current_user.id)
+        user.first_name = form.data['first_name']
+        user.last_name = form.data['last_name']
+        user.zipcode = form.data['zipcode']
+        user.password = form.data['password']
+
+        db.session.commit()
+        return user.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
---
+++ react-app/src/components/UserMemberShipPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UserMemberShipPage() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default UserMemberShipPage
+
