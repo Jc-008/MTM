817bae65adc209ec56dfa3a0df9bb4fdcc9c1238
jonathan <jonathanchan.808@gmail.com>
2021-06-11 15:48:47 -0400

    Have basic styling need to work on search now to search for gyms

--- react-app/src/components/GymPages.js
+++ react-app/src/components/GymPages.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from 'react-router-dom';
 import * as sessionActions from "../store/session"
-// import * as gymReducer from '../store/gyms'
+import * as gymReducer from '../store/gyms'
 import { getOneGym } from '../store/gyms'
+// import gyms from '../store/gyms'
 import {
   Flex,
   Text,
@@ -11,12 +12,14 @@ import {
 } from "@chakra-ui/react"
 
 export default function GymPages() {
-  const gym = useSelector(state => state.gym);
+  const gym = useSelector(state => state.gym.gym);
   const dispatch = useDispatch()
   const { id } = useParams()
+  // const []
 
 
   console.log(gym, '------- this is the gyms')
+  console.log(gym?.name, '------- this is the gyms name')
 
   useEffect(() => {
     dispatch(getOneGym(id))
@@ -24,7 +27,15 @@ export default function GymPages() {
 
   return (
     <>
-      <Text>This is the page for</Text>
+      <Flex
+        h={'97vh'}
+        bg={'lightblue'}
+        align={"center"}
+        justify={"center"}
+      >
+        <Text>This is the page for {gym?.name}</Text>
+
+      </Flex>
 
     </>
   )
